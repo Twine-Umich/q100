e2a0662f363743c3d226feac7e33060ce37fe165
Shibo Chen <chshibo@umich.edu>
2020-11-08 22:41:33 -0500

    Added comments to tile

--- src/main/scala/Tile.scala
+++ src/main/scala/Tile.scala
@@ -14,32 +14,40 @@ class Tile extends SimpleChiselModule with Q100Params{
     val ctrl = IO(new DecoupledIOCtrl(3,3))
     
     // First declare modules
-    val colSelect0 = Module(new ColSelect(4, 0))
-    val colSelect1 = Module(new ColSelect(4, 1))
-    val colSelect2 = Module(new ColSelect(4, 2))
+    val colSelect0 = Module(new ColSelect(4, 0)) // This gives us col 0
+    val colSelect1 = Module(new ColSelect(4, 1)) // This gives us col 1
+    val colSelect2 = Module(new ColSelect(4, 2)) // This gives us col 2
 
-    val boolGen = Module(new BoolGen(BoolGenOP.GT, 6.U))
+    // This takes input and compares it to 6. True if the result is greater than 6
+    val boolGen = Module(new BoolGen(BoolGenOP.GT, 6.U)) 
 
+    // These modules filter out row based on the boolgen results
     val colFilter0 = Module(new ColFilter)
     val colFilter1 = Module(new ColFilter)
     val colFilter2 = Module(new ColFilter)
 
+    // Add the two inputs together
     val alu = Module(new ALU(AluOp.ADD))
 
+    // The aggregatpr
     val aggregate = Module(new Aggregator)
 
+    // Connect inputs to colselects
     in >>> colSelect0
     in >>> colSelect1
     in >>> colSelect2
 
+    // Filter out the rows where the value in column 0 is greater than 6 or (Col 0 > 6)
     colSelect0 >>> boolGen
 
     boolGen >>> colFilter0
     SimpleChiselBundle(colSelect1.out.dat, boolGen.out.cmp, colSelect1.out.EOF) >>> colFilter1
     SimpleChiselBundle(colSelect2.out.dat, boolGen.out.cmp, colSelect2.out.EOF) >>> colFilter2
 
+    // 3. Add column 1 and column 2 together (col1 + col2)
     SimpleChiselBundle(colFilter1.out.dat, colFilter2.out.dat, colFilter1.out.EOF) >>> alu
 
+    // 4. Aggregate the results in step 3 by col 0.
     SimpleChiselBundle(colFilter0.out.dat, alu.out.dat, alu.out.EOF) >>> aggregate
 
     aggregate >>> out
